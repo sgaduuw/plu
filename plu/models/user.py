3348e7129e10c58e88514b72bae5f1dbe6a7767f
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2023-12-01 13:37:56 +0100

    tweak model

--- plu/models/user.py
+++ plu/models/user.py
@@ -14,16 +14,9 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, index=True)
     email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    password = Column(String)
     group_id = Column(Integer, ForeignKey("groups.id"))
 
     group = relationship("Group", back_populates="users")
     transactions = relationship("Transactions", back_populates="initiator")
 
-    @classmethod
-    def set_password(self, password):
-        print(f"{password}")
-        self.hashed_password = pwd_context.hash(password)
-
-    def verify_password(self, password):
-        return pwd_context.verify(password, self.hashed_password)
